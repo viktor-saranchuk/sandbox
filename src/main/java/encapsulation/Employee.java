ec936a3a6d303c6b0f4f76a59c874eeae6cf6ced
viktor-saranchuk <saranchukvm87@gmail.com>
2024-02-10 10:35:56 +0100

    Add unit test for Employee

---
+++ src/main/java/encapsulation/Employee.java
@@ -0,0 +1,39 @@
+package encapsulation;
+
+public class Employee {
+
+    public static int num;
+
+    private String name;
+    private int age;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void replaceName(Employee employee) {
+        // Encapsulation flaw
+        employee.name = this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/main/java/study/App.java
+++ src/main/java/study/App.java
@@ -1,19 +1,18 @@
 package study;
 import java.io.IOException;
 
-import encaapsulation.Employee;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static int a;
     public static void main( String[] args ) throws IOException
     {
-        Employee emp = new Employee("Ordinar Guy");
-        Employee boss = new Employee("Boss");
+        System.out.println("Started");
 
-        System.out.println(boss.isEquals(emp));
+        System.out.println(a);
     }
+
 }
