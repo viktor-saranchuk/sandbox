fe5763e19b9a514ec819230ae5b4cbe3ee351d9a
viktor-saranchuk <saranchukvm87@gmail.com>
2024-02-11 20:05:24 +0100

    Add maven javadoc plugin. to run 'mvn javadoc:javadoc'

--- src/main/java/study/App.java
+++ src/main/java/study/App.java
@@ -7,12 +7,14 @@ import java.io.IOException;
  */
 public class App 
 {
-    public static int a;
+    public Integer i;
     public static void main( String[] args ) throws IOException
     {
-        System.out.println("Started");
+        System.out.println("App start");
 
-        System.out.println(a);
+        System.out.println(new App().i);
+
+        System.out.println("App finish");
     }
 
 }
