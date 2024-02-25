9d466c3bf4b5e3a33f2583bb702fb26c50f10282
viktor-saranchuk <saranchukvm87@gmail.com>
2024-02-25 18:46:29 +0100

    played with lambdas and method references. Find Method references a bit convoluted topic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,16 @@
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.EventListener;
+import java.util.stream.Stream;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 import inheritance.Do;
+import inheritance.InstanceClass;
 import inheritance.TimePrinter;
 import inheritance.Worker;
 /**
@@ -13,7 +21,60 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-        playLambda2();
+        playMethodReferences2();
+    }
+
+    public static void playMethodReferences2() {
+        var test = "3".transform(BigInteger::new);
+        System.out.println(test.getClass());
+    }
+
+    public static void playLambda4() {
+        Timer t = null;
+
+        for (var i = 0; i < 10; i++) {
+
+            //illegal. i must be final or effectively final
+            //ActionListener listener = event -> System.out.println("CTRL+C or CMD+C. " + i);
+
+            //legal
+            var b = i;
+            ActionListener listener = event -> System.out.println("CTRL+C or CMD+C. " + b);
+            if (t == null) {
+                t = new Timer(500, listener);
+                continue;
+            }
+
+            t.addActionListener(listener);
+        }
+
+        t.start();
+
+
+        while (true) {
+            
+        }
+    }
+
+    public static void playLambda3() {
+        var text = "test";
+
+        // text can not be reassigned in the code. So it must be final or effectively final
+        ActionListener listener = event -> System.out.println("CTRL+C or CMD+C. " + text);
+
+        new Timer(1000, event -> System.out.println("CTRL+C or CMD+C. %s")).start();
+        //text = "stop";
+        while (true) {
+            
+        }
+    }
+
+    public static void playMethodReferences() {
+        String[] arr = new String[]{"1", "3", "2"};
+
+        Arrays.sort(arr, InstanceClass::compareTo);
+
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
     }
 
     public static void playLambda2() {
---
+++ src/main/java/inheritance/InstanceClass.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class InstanceClass {
+
+    public static int compareTo(String x, String y) {
+        return -x.compareTo(y);
+    }
+}
